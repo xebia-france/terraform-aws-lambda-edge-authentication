244b1d70a5d3e82ff7bf441f7be84a393ab16b67
Pablo Lopez <plopez@xebia.fr>
2020-06-08 15:50:18 +0200

    Revert "Simplfy for test"
    This reverts commit b6bb7dbdd5e066e534eea234a3ce6f2b633835ee.

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import {Redirect} from './redirect';
+
+exports.handler = async (event, context, callback) => {
+    const redirection = new Redirect();
+    return redirection.handler(event, context, callback);
+};
