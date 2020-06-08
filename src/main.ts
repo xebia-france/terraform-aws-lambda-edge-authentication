e60aedbc21b37d4a8d79fa39c986a98c3ba0c978
Pablo Lopez <plopez@xebia.fr>
2020-06-08 15:45:09 +0200

    Simplfy for test

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import {Redirect} from './redirect';
-
-exports.handler = async (event, context, callback) => {
-    const redirection = new Redirect();
-    return redirection.handler(event, context, callback);
-};
